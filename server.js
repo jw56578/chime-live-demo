347bb2a92cfa37f1d0264d2494f746107f72f314
Jon Woo <jw56578@gmail.com>
2020-09-18 16:53:13 -0500

    correct port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { v4: uuidv4 } = require('uuid');
 const meetingTable = {};
 
 // Use local host for application server
-const host = `127.0.0.1:8080${process.env.PORT}`;
+const host = `127.0.0.1:${process.env.PORT}`;
 
 // Load the contents of the web application to be used as the index page
 const indexPage = fs.readFileSync(`dist/${process.env.npm_config_app || 'meetingV2'}.html`);
